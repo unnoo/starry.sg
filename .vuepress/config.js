a32b41d40ce43950dcfb1392ed3aa0bdd5a05d20
wulujia <wulujia@gmail.com>
2024-03-27 13:39:45 +0800

    fix path

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     sidebarDepth: 0,
     nav: [
       { text: 'About Us', link: '/' },
-      { text: 'Privacy', link: '/privacy' },
+      { text: 'Privacy', link: '/en/privacy' },
     ],
     markdown: {
       lineNumbers: true,              // 让代码块显示行号
