c622ab9aaaf5cfa5110553687cc8d95a73a21dce
wulujia <wulujia@gmail.com>
2024-03-27 13:28:43 +0800

    u

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,10 +12,6 @@ module.exports = {
       lineNumbers: true,              // 让代码块显示行号
       extractHeaders: ['h2', 'h3'],   // 从 MarkDown 文档中提取哪几级标题，保存到 this.$page.headers
       toc: { includeLevel: [2, 3] },  // MarkDown 文档中，用 [[toc]] 标签建立目录时，收集哪几级标题
-    },
-    sidebar: [
-      '/',
-      ['/about/about', 'About'],
-    ]
+    }
   }
 }
